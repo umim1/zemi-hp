12cdf54a8a6e4de37ce199de641aa8d8c5dc08cc
sota3 <sota38911@gmail.com>
2025-02-18 21:56:53 +0900

    フォームの記入欄を大きく修正

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, SubmitField
+from wtforms import StringField, EmailField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
 # 入力クラス
 class Input(FlaskForm):
-    content = StringField("記入欄 :", validators=[DataRequired(message='必須入力です')])
+    content = TextAreaField("記入欄 :", validators=[DataRequired(message='必須入力です')],
+                            render_kw={"rows":5, "cols":50})
     email = EmailField("メールアドレス :", validators=[DataRequired(message='メールアドレスを入力してください'), Email(message='正しいメールアドレスの形式ではありません')])
     submit = SubmitField("送信")
